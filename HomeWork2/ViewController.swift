c3b1cc69870caefcdc0a518d4633e33f40df6b25
Виталий <its.vint@ya.ru>
2020-10-07 21:46:51 +0300

    Initial commit

---
+++ HomeWork2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2
+//
+//  Created by Виталий Овсянников on 07.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
