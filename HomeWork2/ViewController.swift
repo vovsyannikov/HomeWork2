d70b6835f2f7a232dd95bc893d41bcc11df4c4ce
Виталий <its.vint@ya.ru>
2020-10-20 20:15:33 +0300

    Готова верстка для задания 1

--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func download(_ sender: Any) {
+        
+    }
+    
 }
 
