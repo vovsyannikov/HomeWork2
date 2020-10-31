8ce7dfbcf7fab3daf1abc94fdaf2811321fcc606
Виталий <its.vint@ya.ru>
2020-10-31 23:10:10 +0300

    Обновление для интеграции других сервисов

--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -17,29 +17,43 @@ class ViewController: UIViewController  {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        vStack.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
-        vStack.distribution = .fillEqually
-        vStack.axis = .vertical
-        vStack.center = view.center
+        setUpVStack()
+        
         
         vStack.addArrangedSubview(createFBLoginButton())
         vStack.addArrangedSubview(createFBShareButton())
+        addSpacer(for: "ВКонтакте")
         
         
         view.addSubview(vStack)
         
     }
     
+    func setUpVStack(){
+        
+        vStack.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        vStack.distribution = .fillEqually
+        vStack.axis = .vertical
+        vStack.center = view.center
+        
+        addSpacer(for: "FaceBook")
+    }
+    func addSpacer(for name: String) {
+        let spacerLabel = UILabel()
+        spacerLabel.text = name
+        
+        vStack.addArrangedSubview(spacerLabel)
+    }
+    
     func createFBLoginButton() -> FBLoginButton {
         let fbLogin = FBLoginButton()
         
         fbLogin.delegate = self
-        fbLogin.permissions = ["email"]
+        fbLogin.permissions = ["email", "public_profile"]
         fbLogin.center = view.center
         
         return fbLogin
     }
-    
     func createFBShareButton() -> FBShareButton {
         let fbShare = FBShareButton()
         let fbContentToShare: SharingContent = ShareLinkContent()
@@ -56,7 +70,20 @@ class ViewController: UIViewController  {
 extension ViewController: LoginButtonDelegate {
     func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
         
-        print("result = \(String(describing: result?.grantedPermissions)), error = \(String(describing: error))")
+        print("_____________________________")
+        
+        switch result {
+        case .some(let res):
+            print(res.grantedPermissions)
+        case .none:
+            print("++++++++")
+        }
+        
+        if error != nil {
+            print("Error -> \(error!)")
+        }
+        
+        print("_____________________________")
     }
     
     func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
@@ -65,7 +92,6 @@ extension ViewController: LoginButtonDelegate {
     
     
 }
-
 extension ViewController: SharingDelegate{
     func sharer(_ sharer: Sharing, didCompleteWithResults results: [String : Any]) {
         for res in results{
