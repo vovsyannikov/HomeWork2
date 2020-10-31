8b33aaf211525b10fb1b1b9256a00d182a042a65
Виталий <its.vint@ya.ru>
2020-10-31 22:12:48 +0300

    Готова кнопка FB

--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController  {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        createFBButton()
+        
     }
-
-
+    
+    func createFBButton(){
+        let fbLogin = FBLoginButton()
+        fbLogin.delegate = self
+        fbLogin.permissions = ["email"]
+        fbLogin.center = view.center
+        
+        view.addSubview(fbLogin)
+    }
+    
 }
 
+extension ViewController: LoginButtonDelegate {
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        
+        print("result = \(result?.grantedPermissions), error = \(error)")
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        print("Did Logout ")
+    }
+    
+    
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '14.0'
+
+target 'HomeWork2' do
+  use_frameworks!
+
+  pod 'FBSDKCoreKit'
+  pod 'FBSDKLoginKit'
+  pod 'FBSDKShareKit'
+  pod 'FBSDKPlacesKit'
+
+end
