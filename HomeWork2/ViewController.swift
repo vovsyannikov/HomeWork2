80615763dcabb13a22a4b4c40cca4c8605eb6e68
Виталий <its.vint@ya.ru>
2020-12-03 00:15:55 +0300

    Готово (?) определение лица

--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -6,14 +6,100 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    private let cameraSession = AVCaptureSession()
+    private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: self.cameraSession)
+    private let videoDataOutput = AVCaptureVideoDataOutput()
+    private var faceLayers = [CAShapeLayer]()
+    
+    // MARK: Standart Funcs
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupCamera()
+        cameraSession.startRunning()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.previewLayer.frame = self.view.frame
+    }
+    
+    // MARK: Created funcs
+    /// Detecting available camera
+    private func setupCamera(){
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .front)
+        if let device = deviceDiscoverySession.devices.first{
+            if let deviceInput = try? AVCaptureDeviceInput(device: device) {
+                if cameraSession.canAddInput(deviceInput) {
+                    cameraSession.addInput(deviceInput)
+                    
+                    setupPreview()
+                }
+            }
+        }
+    }
+    
+    /// Create video output to show
+    private func setupPreview(){
+        self.previewLayer.videoGravity = .resizeAspectFill
+        self.view.layer.addSublayer(self.previewLayer)
+        self.previewLayer.frame = self.view.frame
+        
+        self.videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
+        self.videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "Camera queue"))
+        
+        self.cameraSession.addOutput(videoDataOutput)
+        let videoConnection = self.videoDataOutput.connection(with: .video)
+        videoConnection?.videoOrientation = .portrait
+    }
+    
+    private func handleFaceDetectionObservations(_ observations: [VNFaceObservation]) {
+        for observation in observations {
+            let faceRectConverted = self.previewLayer.layerRectConverted(fromMetadataOutputRect: observation.boundingBox)
+            let faceRectPath = CGPath(rect: faceRectConverted, transform: nil)
+            
+            let faceLayer = CAShapeLayer()
+            faceLayer.path = faceRectPath
+            faceLayer.fillColor = UIColor.clear.cgColor
+            faceLayer.strokeColor = UIColor.red.cgColor
+            
+            self.faceLayers.append(faceLayer)
+            self.view.layer.addSublayer(faceLayer)
+            
+        }
+    }
+    
 }
 
+// MARK: - AVCapture Extension
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        let faceDetectionRequest = VNDetectFaceLandmarksRequest { (request, error) in
+            DispatchQueue.main.async {
+                self.faceLayers.forEach({ $0.removeFromSuperlayer() })
+                
+                if let observations = request.results as? [VNFaceObservation] {
+                    self.handleFaceDetectionObservations(observations)
+                }
+            }
+        }
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: imageBuffer, orientation: .leftMirrored)
+        
+        do {
+            try imageRequestHandler.perform([faceDetectionRequest])
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+}
